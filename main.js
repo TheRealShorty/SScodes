bf58eabe4dfee462679d1e4a6cb3110229c46331
Shorty <cshaw268@gmail.com>
2023-11-11 16:59:11 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+let z = 0;
+let y = 12;
+let x = 600;
+let isDone = false;
+
+z = (x * y * 9) / 2000
+
+// TODO: fix this variable naming scheme
+// while(!isDone){
+//     console.log(`Loop: ${x}`);
+//     z = x + y ;
+
+//     if(x > 5){
+//         isDone = true;
+//     }
+// }
+// console.log(`Pi: ${Math.PI} == ${z} / ${z = 5}.`)
+
+console.log(`Done: ${z}`);
+// pi=5;
